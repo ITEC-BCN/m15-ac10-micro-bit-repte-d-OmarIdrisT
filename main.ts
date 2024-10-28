8c35828141ebe77bcabf894e0980b0157c72716c
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2024-10-28 16:19:12 +0100

    Acabat

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
-basic.forever(function on_forever() {
+basic.forever(function () {
     if (input.temperature() > 22) {
         basic.showString("Calor!")
     } else {
         basic.showString("Fred!")
     }
-    
     if (input.lightLevel() > 200) {
         music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.LoopingInBackground)
     } else if (input.lightLevel() < 50) {
         music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.LoopingInBackground)
     }
-    
 })
